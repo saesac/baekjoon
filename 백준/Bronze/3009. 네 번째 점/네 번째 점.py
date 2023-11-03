77642f5433875bf69a05a9754424c6ef911f7dfd
Seungwoo Cho <saesacdev@gmail.com>
2023-11-03 21:19:02 +0900

    [Bronze III] Title: 네 번째 점, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,13 @@
+xlist=[]
+ylist=[]
+for i in range(0,3):
+    x,y=map(int,input().split())
+    if x in xlist:
+        xlist.remove(x)
+    else:
+        xlist.append(x)
+    if y in ylist:
+        ylist.remove(y)
+    else:
+        ylist.append(y)
+print(xlist[0],ylist[0])
